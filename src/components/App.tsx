083f4ee904f2ca74c67d3d681d54e34dc7d35d74
victor <vhlongon@gmail.com>
2021-02-22 01:26:18 +0100

    add loading state for component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteTodo, fetchTodos } from '../actions/actions';
 import { StoreState } from '../reducers/rootReducer';
 
 const App = (): JSX.Element => {
+  const [loading, setLoading] = useState(false);
   const todos = useSelector(({ todos }: StoreState) => todos);
   const dispatch = useDispatch();
 
@@ -13,16 +14,24 @@ const App = (): JSX.Element => {
 
   useEffect(() => {
     dispatch(fetchTodos());
+    setLoading(true);
   }, [dispatch]);
+
+  useEffect(() => {
+    if (todos.length) {
+      setLoading(false);
+    }
+  }, [todos]);
 
   return (
     <div>
       <h1>Todos</h1>
+      {loading && <div>Loading...</div>}
       <ul>
         {todos.map(({ id, title, completed }) => (
           <li key={id}>
+            <button onClick={handleClick(id)}>delete </button>
             {title} {completed && ' - completed'}
-            <button onClick={handleClick(id)}>Remove</button>
           </li>
         ))}
       </ul>
